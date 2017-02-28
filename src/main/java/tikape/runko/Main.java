74275db3c28493e8983cba3912bc971934a3dfd2
Aarne Rissanen <aarne.rissanen@gmail.com>
2017-02-28 16:16:03 +0200

    Heroku-versio

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -11,12 +11,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
         
         String jdbcOsoite = "jdbc:sqlite:tietokanta.db";
         
         if (System.getenv("DATABASE_URL") != null) {
             jdbcOsoite = System.getenv("DATABASE_URL");
-        } 
+        }
+        
+        
 
         Database database = new Database(jdbcOsoite);
         
